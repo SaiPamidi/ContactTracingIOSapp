0c35ace8f632a1db5702d5db247ed1ae9985a13c
Sai Swaroop Pamidi <saiswaroop.pamidi@gmail.com>
2020-07-03 19:32:15 -0400

    Initial Commit

---
+++ ContactTracing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactTracing
+//
+//  Created by Sai swaroop Pamidi on 7/3/20.
+//  Copyright © 2020 Sai swaroop Pamidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
